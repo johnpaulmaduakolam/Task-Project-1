e020d1b41a8d71140e739c81907d4ba50c1c2659
johnpaulmaduakolam <127172810+johnpaulmaduakolam@users.noreply.github.com>
2025-03-25 05:19:26 +0100

    Add files via upload

---
+++ Assessment.py
@@ -0,0 +1,41 @@
+Python 3.13.2 (v3.13.2:4f8bb3947cf, Feb  4 2025, 11:51:10) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> #Ask the user's to input two numbers:
+>>> 
+>>> #Perform Operation(Addition)
+>>> 
+>>> num1=20
+>>> 
+>>> num2=30
+>>> 
+>>> print(num1+num2)
+50
+>>> 
+>>> #Perform Operation(Subtraction)
+>>> 
+>>> num1=30
+>>> 
+>>> num2=15
+>>> 
+>>> print(num1-num2)
+15
+>>> 
+>>> 
+>>> #Perform Operation(Multiplication)
+>>> 
+>>> num=10
+>>> 
+>>> num2=5
+>>> 
+>>> print(num*num2)
+50
+>>> 
+>>> #Perform Operation(Division)
+>>> 
+>>> num1=40
+>>> 
+>>> num2=20
+>>> 
+>>> print(num1/num2)
+2.0
